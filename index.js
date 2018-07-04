4643025de92bd9a209efcd17cb9d77ffd5b3af98
Andrew Gierens <agierens@jdlf.com.au>
2018-07-04 14:22:31 +1000

    - Added: Basic time command

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const Telegraf = require('telegraf')
+const moment = require('moment-timezone');
+const cityTimezones = require('city-timezones');
 
-const bot = new Telegraf('');
+const bot = new Telegraf('612890451:AAFtuslgkjjT7N5-7FtODllZlECXzjXQcK4');
 
 bot.start((ctx) => ctx.reply('Welcome!'));
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.command('time', (ctx) => {
+  var cityName = ctx.message.text.replace('/time ', '');
+  const cityLookup = cityTimezones.lookupViaCity(cityName)[0];
+  ctx.reply(moment().tz(cityLookup.timezone).format('MMMM Do YYYY, h:mm:ss a'));
+});
 
 bot.startPolling();
