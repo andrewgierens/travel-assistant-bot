f9e2449a7a8396cda9dddf7b6b70b9ddad8c02f9
agierens <andrew.gierens@bigpond.com>
2018-07-04 19:23:27 +1000

    - Added: Travis and time provider

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const Telegraf = require('telegraf')
+const Telegraf = require('telegraf');
+const commandParts = require('telegraf-command-parts');
 
 const TimeProvider = require('./providers/time-provider');
 
 const bot = new Telegraf(process.env.TRAVEL_BOT_TOKEN);
-
-bot.start((ctx) => ctx.reply('Welcome!'));
-bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.use(commandParts());
 bot.command('time', (ctx) => {
-  var cityName = ctx.message.text.replace('/time ', '');
-  ctx.reply(TimeProvider.getTimeFromCity(cityName));
+  const commandArgs = ctx.state.command.splitArgs;
+  if (!commandArgs || commandArgs.length === 0 || (commandArgs.length === 1 && !commandArgs[0])) {
+    ctx.reply('I\'m sorry, you need to tell me a city or country!');
+    return;
+  }
+
+  if (!commandArgs || commandArgs.length > 1) {
+    ctx.reply('I\'m sorry, I can only tell you the time for one city/country!');
+    return;
+  }
+
+  var cityName = commandArgs[0];
+  var formattedTime = TimeProvider.getFormattedTimeFromCity(cityName);
+  if (!formattedTime) {
+    ctx.reply('I\'m sorry, I can\'t find that city/country!');
+    return;
+  }
+
+  ctx.replyWithMarkdown(formattedTime);
 });
 
 bot.startPolling();
--- providers/time-provider.js
+++ providers/time-provider.js
@@ -1,10 +1,21 @@
 const moment = require('moment-timezone');
 const cityTimezones = require('city-timezones');
+const Markup = require('telegraf/markup')
 
 class TimeProvider {
   static getTimeFromCity(cityName) {
     const cityLookup = cityTimezones.lookupViaCity(cityName)[0];
-    return moment().tz(cityLookup.timezone).format('MMMM Do YYYY, h:mm:ss a');
+    if (cityLookup == null) return null;
+
+    return moment().tz(cityLookup.timezone).format('h:mm:ss a');
+  }
+
+  static getFormattedTimeFromCity(cityName) {
+    const cityLookup = cityTimezones.lookupViaCity(cityName)[0];
+    if (cityLookup == null) return null;
+    
+    const time = moment().tz(cityLookup.timezone).format('h:mm:ss a');
+    return `The time in *${cityLookup.city}, ${cityLookup.country}* is: ${time}`;
   }
 }
 
